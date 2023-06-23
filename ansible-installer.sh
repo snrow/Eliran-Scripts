fe57503fcc60d76689e0ad84c6ff7ee5eefa4033
snrow <eliranbemmaor@gmail.com>
2023-06-23 17:30:53 +0300

    Added ansible , aws-cli , terraform install scripts

---
+++ ansible-installer.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+python3 -m pip install --user ansible
---
+++ aws-cli-installer.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+unzip 
+if [ $? -eq 0 ]; then
+    echo zip is installed
+else
+    sudo apt install unzip
+fi
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
