b4d1edcba5bd352209442dbdd8a495e585d01db9
Eliran Ben Maor <eliranbenmaor@gmail.com>
2023-11-07 08:00:31 +0200

    added K8's master + worker scripts + added the dockeer installer script to be much faster

--- Docker-installer.sh
+++ Docker-installer.sh
@@ -1,32 +1,31 @@
-#!/bin/bash
-# install docker on linux Ubuntu
-# removing any trace of docker 
-sudo apt-get remove docker docker-engine docker.io containerd runc 2>/dev/null
+# Auto Installation of docker on linux.
+# Ubuntu based.
+#
+# Made by Avshalom Hadad + Eliran Ben Maor.
 
-#Set up the repository
+#!/bin/bash set -x set -e
+
+# Set up the repository
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
-#Add Docker’s official GPG key:
+# Add Docker’s official GPG key:
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-#setup the repository
+# Add docker repo
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+# Update and install docker
 sudo apt-get update -y
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-#post install
-#create docker group
-sudo groupadd docker  2>/dev/null
+# Post install - Create 'docker' group
+sudo groupadd docker 2>/dev/null
 
-#Add your user to the docker group
+# Add your user to the docker group
 sudo usermod -aG docker $USER
 
-#activate the changes to group docker
+# Activate the changes to group docker
 newgrp docker
